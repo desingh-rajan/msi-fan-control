c277bad2e1c267e8651e583a49dc201e909d19b2
desinghrajan <desinghrajan@gmail.com>
2026-01-20 10:34:38 +0530

    fix(build): resolve postcss error by checking vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [sveltekit()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
